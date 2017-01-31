077235277edd6d5abe7e749c161ea4d1fdc375d6
danielasocas <danielasocasgil@gmail.com>
2017-01-31 04:43:16 +0000

    Created index.html, 'It works' and list of comments(created by admin).

--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Comment
+
+
+def index(request):
+    lasts_comment_list = Comment.objects.order_by('-created_date')[:5]
+    comment_list = Comment.objects.all()
+    context = {'lasts_comment_list': lasts_comment_list}
+    return render(request, 'post/index.html', context)
