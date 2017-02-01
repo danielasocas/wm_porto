ba9154c9df8df024b3cb6b42cc9100e7f855ac98
danielasocas <danielasocasgil@gmail.com>
2017-02-01 18:00:39 +0000

    comments working, they are added in other page.

--- post/urls.py
+++ post/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- post/views.py
+++ post/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Comment
-
+from .forms import CommentForm
 
 def index(request):
     lasts_comment_list = Comment.objects.order_by('-created_date')[:5]
     comment_list = Comment.objects.all()
     context = {'lasts_comment_list': lasts_comment_list}
     return render(request, 'post/index.html', context)
+    
+def add_comment_to_post(request):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+    else:
+        form = CommentForm()
+    return render(request, 'post/add_comment_to_post.html', {'form': form})
